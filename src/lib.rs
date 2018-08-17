fb6a5d5461960aaf663f642051619b1414ca7754
BlueTeleco <lucas.lucario@gmail.com>
2018-08-17 20:17:13 +0200

    Can output window themes, without variables

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@ mod yaml;
 use std::io;
 use std::fs;
 use yaml::*;
+use yaml_rust::Yaml;
 
 pub fn run(input: String, output: String, theme: String) {
     let path = format!("themes/{}", theme);
-    println!("Input: {} --- Output: {} --- Theme: {}", input, output, format_theme(path));
+    println!("Input: {} --- Output: {} --- Theme:\n\n{}", input, output, window_theme(path));
 }
 
 pub fn list() -> io::Result<()> {
@@ -27,10 +28,8 @@ pub fn list() -> io::Result<()> {
             Err(_e) => continue,
         };
 
-        let desc = match get_yaml_str(&theme[0], "meta", "description", "") {
-            Some(s) => s,
-            None => "Description not found".to_string(),
-        };
+        let desc = get_yaml_str(&theme[0], "meta", "description", "")
+                    .unwrap_or("Description not found".to_string());
 
         let name = match entry.file_name().into_string() {
             Ok(s) => s,
@@ -41,8 +40,34 @@ pub fn list() -> io::Result<()> {
     Ok(())
 }
 
-fn format_theme(theme: String) -> String {
-    let padding = "#".repeat(8);
-    let result = format!("{0} {1} {0}\n", padding, "i3themes configuration");
+fn window_theme(path: String) -> String {
+    let win_types = ["focused", "unfocused", "focused_inactive", "urgent"];
+    let padding = "#".repeat(26);
+    let mut result = format!("{0} {1} {0}\n\n", padding, "i3themes window configuration");
+
+    let theme = load_yaml(&path).unwrap_or_else(|_e| {
+            println!("Error loading the theme, try again or submit a bug report");
+            vec![Yaml::BadValue]
+    });
+
+    for e in &win_types {
+        let color = wstate_colors(&theme[0], e).unwrap_or_else(|| {
+                println!("Error loading the theme, try again or submit a bug report");
+                "".to_string()
+        });
+        result = result + &color;
+    }
     result
+}
+
+fn wstate_colors(theme: &Yaml, state: &str) -> Option<String> {
+    let win_elements = ["border", "background", "text", "indicator"];
+    let mut elem_colors: [String;4] = [String::new(), String::new(), String::new(), String::new()];
+
+    for n in 0..4 {
+        elem_colors[n] = get_yaml_str(theme, "window_colors", state, &win_elements[n])?;
+    }
+
+    let colors = format!("client.{0: <20} {1: <15} {2: <15} {3: <15} {4: <15}\n", state, &elem_colors[0], &elem_colors[1], &elem_colors[2], &elem_colors[3]);
+    Some(colors)
 }

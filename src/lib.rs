7340ceb6b2f1c1b5cf9efe9552f8caa957d8d919
BlueTeleco <lucas.lucario@gmail.com>
2018-08-18 17:31:21 +0200

    Can output bar themes, without variables

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use yaml_rust::Yaml;
 
 pub fn run(input: String, output: String, theme: String) {
     let path = format!("themes/{}", theme);
-    println!("Input: {} --- Output: {} --- Theme:\n\n{}", input, output, window_theme(path));
+    println!("Input: {} --- Output: {} --- Theme:\n\n{} \n\n{}", input, output, window_theme(&path), bar_theme(&path));
 }
 
 pub fn list() -> io::Result<()> {
@@ -40,12 +40,12 @@ pub fn list() -> io::Result<()> {
     Ok(())
 }
 
-fn window_theme(path: String) -> String {
-    let win_types = ["focused", "unfocused", "focused_inactive", "urgent"];
+fn window_theme(path: &str) -> String {
     let padding = "#".repeat(26);
     let mut result = format!("{0} {1} {0}\n\n", padding, "i3themes window configuration");
+    let win_types = ["focused", "unfocused", "focused_inactive", "urgent"];
 
-    let theme = load_yaml(&path).unwrap_or_else(|_e| {
+    let theme = load_yaml(path).unwrap_or_else(|_e| {
             println!("Error loading the theme, try again or submit a bug report");
             vec![Yaml::BadValue]
     });
@@ -55,19 +55,69 @@ fn window_theme(path: String) -> String {
                 println!("Error loading the theme, try again or submit a bug report");
                 "".to_string()
         });
-        result = result + &color;
+        result.push_str(&color);
     }
-    result
+    result + "\n" + &"#".repeat(83)
 }
 
 fn wstate_colors(theme: &Yaml, state: &str) -> Option<String> {
     let win_elements = ["border", "background", "text", "indicator"];
-    let mut elem_colors: [String;4] = [String::new(), String::new(), String::new(), String::new()];
+    let mut elem_colors = [String::new(), String::new(), String::new(), String::new()];
 
     for n in 0..4 {
         elem_colors[n] = get_yaml_str(theme, "window_colors", state, &win_elements[n])?;
     }
 
     let colors = format!("client.{0: <20} {1: <15} {2: <15} {3: <15} {4: <15}\n", state, &elem_colors[0], &elem_colors[1], &elem_colors[2], &elem_colors[3]);
+    Some(colors)
+}
+
+fn bar_theme(path: &str) -> String {
+    let padding = "#".repeat(26);
+    let mut result = format!("{0} {1} {0}\n\n", padding, "i3themes bar configuration");
+    result.push_str("\tcolor {\n");
+    let bar_types = ["focused_workspace", "active_workspace", "inactive_workspace", "urgent_workspace"];
+
+    let theme = load_yaml(path).unwrap_or_else(|_e| {
+            println!("Error loading the theme, try again or submit a bug report");
+            vec![Yaml::BadValue]
+    });
+    
+    let global = bglobal_colors(&theme[0]).unwrap_or_else(|| {
+            println!("Error loading the theme, try again or submit a bug report");
+            "".to_string()
+    });
+    result.push_str(&global);
+
+    for e in &bar_types {
+        let color = bstate_colors(&theme[0], e).unwrap_or_else(|| {
+                println!("Error loading the theme, try again or submit a bug report");
+                "".to_string()
+        });
+        result.push_str(&color);
+    }
+
+    result.push_str("\t}\n");
+    result + "\n" + &"#".repeat(80)
+}
+
+fn bglobal_colors(theme: &Yaml) -> Option<String> {
+    let separator = get_yaml_str(theme, "bar_colors", "separator", "")?;
+    let background = get_yaml_str(theme, "bar_colors", "background", "")?;
+    let statusline = get_yaml_str(theme, "bar_colors", "statusline", "")?;
+
+    let colors = format!("\t\tseparator {}\n\t\tbackground {}\n\t\tstatusline {}\n", separator, background, statusline);
+    Some(colors)
+}
+
+fn bstate_colors(theme: &Yaml, state: &str) -> Option<String> {
+    let win_elements = ["border", "background", "text"];
+    let mut elem_colors = [String::new(), String::new(), String::new()];
+
+    for n in 0..3 {
+        elem_colors[n] = get_yaml_str(theme, "bar_colors", state, &win_elements[n])?;
+    }
+
+    let colors = format!("\t\t{0: <20} {1: <15} {2: <15} {3: <15} \n", state, &elem_colors[0], &elem_colors[1], &elem_colors[2]);
     Some(colors)
 }

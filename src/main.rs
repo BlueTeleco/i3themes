3ff14d443beacf1e1edb4a1011908c8bd9ca0d02
lopukhov <12306261+lopukhov@users.noreply.github.com>
2019-08-27 19:48:36 +0200

    Find path of config file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 extern crate i3themes;
 
+extern crate dirs;
+
 #[macro_use]
 extern crate clap;
 use clap::App;
+
+use std::process;
+use std::path::PathBuf;
 
 fn main() {
     let yml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yml).get_matches();
 
+    let xdg_location = home_subfile(".config/i3/config");
+    let i3h_location = home_subfile(".i3/config");
+    let etc_location = PathBuf::from("/etc/i3/config");
+
     if let Some(matches) = matches.subcommand_matches("change") {
-        let config = matches.value_of("config").unwrap_or("default.conf");
+        let config = matches.value_of("config").unwrap_or_else(|| {
+            if xdg_location.exists() {
+                xdg_location.to_str().unwrap()
+            } else if i3h_location.exists() {
+                i3h_location.to_str().unwrap()
+            } else if etc_location.exists() {
+                etc_location.to_str().unwrap()
+            } else {
+                println!("No config file found. See help menu for more options.");
+                process::exit(1);
+            }
+        });
         let output = matches.value_of("output").unwrap_or("out.def");
-        let theme = matches.value_of("theme").unwrap_or("theme");
+        let theme = matches.value_of("theme").unwrap();
         i3themes::change(config, output, theme)
     }
 
@@ -30,5 +50,14 @@ fn main() {
     if let Some(matches) = matches.subcommand_matches("install") {
         let theme = matches.value_of("theme");
         println!("Installing {}", theme.unwrap());
+    }
+}
+
+fn home_subfile(file: &str) -> PathBuf {
+    if let Some(mut home) = dirs::home_dir() {
+        home.push(file);
+        home
+    } else {
+        PathBuf::from("~")
     }
 }
